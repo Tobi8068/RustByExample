7c48aa166263776cb62cd93cb6f434d42efd6ee8
Tobi8068 <hana.dev22@gmail.com>
2024-06-11 16:57:08 +1000

    updated: hello-world

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
     println!("{}, Hello, world!", 32);
     println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+    println!("{number:>5}", number=1);
+    println!("{number:0>width$}", number=1, width=5);
+    println!("My name is {0}, {1} {0}", "Bond", "Alex");
 }
