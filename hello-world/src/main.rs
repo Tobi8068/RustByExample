866c22cbaa19d01c6ba574cb3771b7d15c4f7ed2
Tobi <153672885+Tobi8068@users.noreply.github.com>
2025-01-05 23:34:10 -0500

    updated: debug

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     println!("{number:>5}", number=1);
     println!("{number:0>width$}", number=1, width=5);
     println!("My name is {0}, {1} {0}", "Bond", "Alex");
+    println!("My name is {0}, {1} {0}", "Bond", "Bob");
+    println!("{:?} months in a year.", 12);
     let number: f64 = 1.0;
     let width: usize = 5;
     println!("{number:>width$}");
